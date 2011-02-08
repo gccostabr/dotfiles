738e0acaf09df60a7e9489f7e4813dcb75743bca
Guilherme Costa <gccostabr@gmail.com>
2011-02-07 22:28:49 -0200

    Changed to use a function to set files in place.

--- install.sh
+++ install.sh
@@ -1,3 +1,27 @@
 #!/bin/sh
 
-cp gitconfig ~/.gitconfig
+function setFileInPlace 
+{
+
+  if [ -h $1 ]; then
+    echo "Removing old link for $1"
+    rm $1
+  elif [ -f $1 ]; then
+    echo "Moving $1 to $1.old"
+    mv $1 $1.old
+  fi
+  echo "Linking $2 to $1..."
+  ln -s $2 $1
+
+}
+
+setFileInPlace ~/.gitconfig gitconfig
+setFileInPlace ~/.bash_profile bash_profile
+setFileInPlace ~/.vimrc vimrc
+
+echo
+echo
+echo "Done!"
+echo
+
+
